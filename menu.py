33d864dbdff01cded6c430f497776702e7dc7fff
bmw-dev0p <bmw.dev0p@gmail.com>
2024-11-07 21:31:56 -0500

    completed functions and todos, 15/16 tests passing

---
+++ menu.py
@@ -0,0 +1,62 @@
+menu = [
+    {
+        "id": 1,
+        "food": "Hibachi - Chicken",
+        "price": 11.99
+    },
+    {
+        "id": 2,
+        "food": "Hibachi - Steak",
+        "price": 12.99
+    },
+    {
+        "id": 3,
+        "food": "Hibachi - Shrimp",
+        "price": 13.99
+    },
+    {
+        "id": 4,
+        "food": "Sushi - California Roll",
+        "price": 6.99
+    },
+    {
+        "id": 5,
+        "food": "Sushi - Spicy Tuna Roll",
+        "price": 7.99
+    },
+    {
+        "id": 6,
+        "food": "Sushi - Dragon Roll",
+        "price": 8.99
+    },
+    {
+        "id": 7,
+        "food": "Sushi - Rainbow Roll",
+        "price": 9.99
+    },
+    {
+        "id": 8,
+        "food": "Sushi - Sashimi",
+        "price": 10.99
+    },
+    {
+        "id": 9,
+        "food": "Sushi - Nigiri",
+        "price": 11.99
+    },
+    {
+        "id": 10,
+        "food": "Pork Gyoza",
+        "price": 12.99
+    },
+    {
+        "id": 11,
+        "food": "Edamame",
+        "price": 3.99
+    },
+    {
+        "id": 12,
+        "food": "Miso Soup",
+        "price": 2.99
+    }
+]
